f41eb101c6b155a9353970824a4b4e4fcc4784e9
alitalu7 <55026802+alitalu7@users.noreply.github.com>
2022-06-23 20:27:51 +0300

    Add project files.

---
+++ CSharp2/Program.cs
@@ -0,0 +1,9 @@
+﻿int meal_cost = Convert.ToInt32(Console.ReadLine());
+int tip_percent = Convert.ToInt32(Console.ReadLine());
+int tax_percent = Convert.ToInt32(Console.ReadLine());
+
+double tip = (tip_percent * 0.01) * meal_cost;
+double tax = (tax_percent * 0.01) * meal_cost;
+int total = Convert.ToInt32(tax + tip + meal_cost);
+Console.WriteLine(total);
+Console.ReadLine();
